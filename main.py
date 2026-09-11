7034b0fcaf3e8f863c3c634a8478ba8dc21111bd
ridwanreon <reonrahman8@gmail.com>
2026-09-12 02:23:13 +0600

    Update project

--- main.py
+++ main.py
@@ -9,17 +9,10 @@ from router import auth,admin
 from router.auth import get_current_user
 
 
-from fastapi.middleware.cors import CORSMiddleware
+
 
 
 app = FastAPI()
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=["http://localhost:5173"],
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
 
 
 app.include_router(auth.router)
