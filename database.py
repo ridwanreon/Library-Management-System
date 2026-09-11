47d75f33e00fd2d5372293d0a1494c4c10d3366a
ridwanreon <reonrahman8@gmail.com>
2026-09-12 03:00:55 +0600

    updated

--- database.py
+++ database.py
@@ -4,8 +4,9 @@ from sqlalchemy.ext.declarative import declarative_base
 
 
 
-SQLALCHEMY_DATABASE_URL = "postgresql://postgres.fbminsxrhzjqwiyxpkex:ridwanreon12@aws-0-ap-southeast-1.pooler.supabase.com:5432/postgres"
+# SQLALCHEMY_DATABASE_URL = "postgresql://postgres.fbminsxrhzjqwiyxpkex:ridwanreon12@aws-0-ap-southeast-1.pooler.supabase.com:5432/postgres"
 
+SQLALCHEMY_DATABASE_URL = 'sqlite:///./library.db'
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
 SessionLocal = sessionmaker(autoflush=False, autocommit = False, bind = engine)
