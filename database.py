4b618834efa34c1bb477db08038c2d148a6e3801
ridwanreon <reonrahman8@gmail.com>
2026-09-12 02:48:16 +0600

    updated

--- database.py
+++ database.py
@@ -4,7 +4,7 @@ from sqlalchemy.ext.declarative import declarative_base
 
 
 
-SQLALCHEMY_DATABASE_URL = "postgresql://postgres.tiulpnruyveffzcmqipk:11212233232reon@aws-0-ap-southeast-1.pooler.supabase.com:5432/postgres"
+SQLALCHEMY_DATABASE_URL = "postgresql://postgres.fbminsxrhzjqwiyxpkex:ridwanreon12@aws-0-ap-southeast-1.pooler.supabase.com:5432/postgres"
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
