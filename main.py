4f7c835f881962e5e1ca7476178c8ab39e4a7ac8
ridwanreon <reonrahman8@gmail.com>
2026-09-12 13:14:08 +0600

    update

--- main.py
+++ main.py
@@ -8,12 +8,29 @@ from models import Books,Users,Reservations,IssueRecords
 from router import auth,admin
 from router.auth import get_current_user
 
+from fastapi.middleware.cors import CORSMiddleware
 
 
 
 
 app = FastAPI()
+origins = [
+    "http://localhost:5173",
+    "http://localhost:5174",
+    "http://127.0.0.1:5173",
+    "http://127.0.0.1:5174",
+    "http://localhost:3000",
+    "http://127.0.0.1:3000",
+]
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_origin_regex=r"http://(localhost|127\.0\.0\.1):\d+",
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(auth.router)
 app.include_router(admin.router)
